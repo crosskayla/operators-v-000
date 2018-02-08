5a0d0d691e32f1125e7922a664416bd197feb079
Kayla Cross <kac288@georgetown.edu>
2018-02-08 18:20:48 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if speed < 30 || speed > 60
+  if speed < 40 || speed > 60
     return true
   else
     return false
@@ -8,7 +8,7 @@ end
 
 
 def not_safe?(speed)
-	
+	speed<40 || speed>60? true : false
 end
 	
 
